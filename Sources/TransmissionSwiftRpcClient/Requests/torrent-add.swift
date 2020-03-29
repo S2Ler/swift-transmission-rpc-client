a44ab1beb2e40d7ba29df2f8ade895a981ca376f
Alexander Belyavskiy <diejmon@gmail.com>
2020-03-29 18:38:55 +0300

    Parser torrent-add response

--- Sources/TransmissionSwiftRpcClient/Requests/torrent-add.swift
+++ Sources/TransmissionSwiftRpcClient/Requests/torrent-add.swift
@@ -14,7 +14,38 @@ public struct AddTorrentRequestArguments: Encodable {
 }
 
 public struct AddTorrentResponseArguments: Decodable {
+  private struct TorrentAddedInfo: Decodable {
+    let hashString: String
+    let id: Int
+    let name: String
+  }
 
+  private let torrentAddedInfo: TorrentAddedInfo?
+  private let torrentDuplicateInfo: TorrentAddedInfo?
+  private var unwrappedTorrentInfo: TorrentAddedInfo {
+    if let torrentAddedInfo = torrentAddedInfo {
+      return torrentAddedInfo
+    }
+    else if let torrentDuplicateInfo = torrentDuplicateInfo {
+      return torrentDuplicateInfo
+    }
+    else {
+      preconditionFailure("Contract error. Successful response should have either `torrentAddedInfo` or `torrentDuplicateInfo`")
+    }
+  }
+
+  private enum CodingKeys: String, CodingKey {
+    case torrentAddedInfo = "torrent-added"
+    case torrentDuplicateInfo = "torrent-duplicate"
+  }
+
+  public var isDuplicate: Bool { torrentDuplicateInfo != nil }
+
+  public var hashString: String { unwrappedTorrentInfo.hashString }
+
+  public var id: Int { unwrappedTorrentInfo.id }
+
+  public var name: String { unwrappedTorrentInfo.name }
 }
 
 public extension TransmissionSwiftRpcClient {
